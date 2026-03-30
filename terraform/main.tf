89af0089df20608c616788fabb161b2ff848072e
ReeZeMvp <reezemvp@gmail.com>
2026-03-30 16:12:56 +0200

    Séquence 5 - 1.1.1 Patch error CI lié à l'attribut 'host' dans render_postgres.database.connection_info

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ resource "render_web_service" "adminer" {
 
   env_vars = {
     ADMINER_DEFAULT_SERVER = {
-      value = render_postgres.database.connection_info.host
+      value = render_postgres.database.connection_info.external_connection_string
     }
   }
 }
